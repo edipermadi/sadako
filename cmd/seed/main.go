9d2ea60bba8c7498d1e4bf09c34466462fa0b986
Edi Permadi <edipermadi@gmail.com>
2025-12-24 06:45:25 +0400

    update seed generator

--- cmd/seed/main.go
+++ cmd/seed/main.go
@@ -30,11 +30,33 @@ func main() {
 	}
 	defer func() { _ = file.Close() }()
 
-	_ = grid.Permutation(func(g grid.Grid) bool {
+	grids := grid.Permutation(func(g grid.Grid) bool {
 		v := g.Values()
 		if _, err := fmt.Fprintf(file, "INSERT INTO grids (id, c1, c2, c3, c4, c5, c6, c7, c8, c9) VALUES (%d, %d, %d, %d, %d, %d, %d, %d, %d, %d);\n", g, v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7], v[8]); err != nil {
 			log.Warn().Err(err).Str("filename", filename).Msg("failed to write grid")
 		}
-		return false
+		return true
 	})
+
+	formatFn := func(g grid.Grid) string {
+		if g == 0 {
+			return "NULL"
+		}
+		return fmt.Sprintf("%d", g)
+	}
+
+	prefixed := append([]grid.Grid{0}, grids...)
+	for _, h1 := range prefixed {
+		for _, h2 := range prefixed {
+			for _, v1 := range prefixed {
+				for _, v2 := range prefixed {
+					for _, g := range grid.GenerateGrids([]grid.Grid{h1, h2}, []grid.Grid{v1, v2}, grids) {
+						if _, err := fmt.Fprintf(file, "INSERT INTO positions (g, h1, h2, v1, v2) VALUES (%d, %s, %s, %s, %s);\n", g, formatFn(h1), formatFn(h2), formatFn(v1), formatFn(v2)); err != nil {
+							log.Warn().Err(err).Str("filename", filename).Msg("failed to write grid")
+						}
+					}
+				}
+			}
+		}
+	}
 }
